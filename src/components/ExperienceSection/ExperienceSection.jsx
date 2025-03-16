0901944e8e70b56f97e09afa880fa9f450cc7a7a
Megan Veldhuis <megan.veldhuis@gmail.com>
2025-03-15 23:47:59 -0600

    add dropdown for mobile

--- src/components/ExperienceSection/ExperienceSection.jsx
+++ src/components/ExperienceSection/ExperienceSection.jsx
@@ -5,15 +5,48 @@ import JobDescription from "../JobDescription/JobDescription";
 
 function ExperienceSection() {
   const [selectedCompanyID, setSelectedCompanyID] = useState(0);
+  const [screenWidth, setScreenWidth] = useState(screen.width);
 
   function handleCompanyClick(e) {
-    setSelectedCompanyID(e.target.id);
+    if (e.target.nodeName === "SELECT") {
+      setSelectedCompanyID(e.target.value);
+    } else {
+      setSelectedCompanyID(e.target.id);
+    }
   }
+
+  window.addEventListener("resize", () => {
+    setScreenWidth(screen.width);
+  });
 
   return (
     <section className="experience">
       <h2 className="experience__header">Experience</h2>
       <div className="experience__content">
+        {/* {screenWidth < 768 ? (
+          <select
+            className="experience__select"
+            id="selectJob"
+            name="selectJob"
+            value={selectedCompanyID}
+            onChange={handleCompanyClick}
+          >
+            {jobs.map((job) => (
+              <option
+                className={`experience__option ${
+                  selectedCompanyID == job.id
+                    ? "experience__option--selected"
+                    : ""
+                } ${job.isVolunteer ? "experience__option--volunteer" : ""}`}
+                id={job.id}
+                key={job.id}
+                value={job.id}
+              >
+                {job.company}
+              </option>
+            ))}
+          </select>
+        ) : ( */}
         <nav className="experience__nav">
           {jobs.map((job) => (
             <option
@@ -30,6 +63,8 @@ function ExperienceSection() {
             </option>
           ))}
         </nav>
+        {/* )} */}
+
         <div className="experience__job">
           <JobDescription selectedCompany={jobs[selectedCompanyID]} />
         </div>
