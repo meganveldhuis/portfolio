84b9915a2228c4f39a3f717d3e6f8ea4fce6513c
Megan Veldhuis <megan.veldhuis@gmail.com>
2025-04-27 12:03:27 -0600

    add fonts, update hero and gradients

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,24 +4,39 @@ import linkedInLogo from "../../assets/logos/linkedin_logo.svg";
 import emailLogo from "../../assets/logos/email_logo.svg";
 
 function Hero() {
+  const name = "Megan Veldhuis";
+
+  // Split the name into individual characters and wrap each in a span
+  const animatedName = name
+    .split("")
+    .map((char, index) => (
+      <span key={index}>{char === " " ? "\u00A0" : char}</span>
+    ));
   return (
     <div className="hero">
       <div className="hero__top">
         <p className="hero__text hero__over-title">Hey, I'm</p>
-        <h1 className="hero__text hero__name">Megan Veldhuis</h1>
+        <h1 className="hero__text hero__name anim-text-flow">{animatedName}</h1>
         <p className="hero__text hero__subtitle">
-          I am an analytical, energetic, and detail-oriented electrical
-          engineering graduate, with a stubborn determination for what I am
-          passionate about. I have experience in leadership roles and work well
-          on teams, demonstrated by being an executive in Engineering Student
-          Society and Diversity in Engineering. Being on the DivE team for most
-          of my degree, I have learned how essential diversity is for every
-          team. I am perceptive, attentive, and driven to meet deadlines and to
-          produce high-quality work.
+          I am an analytical, energetic, and detail-oriented{" "}
+          <span>electrical engineering</span> graduate, with a stubborn
+          determination for what I am passionate about. I have experience in{" "}
+          <span className="hero__subtitle">leadership</span> roles and work well
+          on teams, demonstrated by being an executive in{" "}
+          <span className="hero__subtitle">Engineering Students' Society</span>{" "}
+          and <span className="hero__subtitle">Diversity in Engineering</span>.
+          Being on the DivE team for most of my degree, I have learned how
+          essential diversity is for every team. I am{" "}
+          <span>perceptive, attentive</span>, and{" "}
+          <span>driven to meet deadlines</span> and to{" "}
+          <span>produce high-quality work</span>.
         </p>
       </div>
       <div className="hero__contact">
-        <a href="mailto:megan.veldhuis@gmail.com" className="gradient-box">
+        <a
+          href="mailto:megan.veldhuis@gmail.com"
+          className="gradient-box gradient-box--blue"
+        >
           <button className="hero__button">
             <img src={emailLogo} />
             Send me an email
@@ -29,14 +44,17 @@ function Hero() {
         </a>
         <a
           href="https://www.linkedin.com/in/megan-veldhuis/"
-          className="gradient-box"
+          className="gradient-box gradient-box--purple"
         >
           <button className="hero__button">
             <img src={linkedInLogo} />
             Connect on LinkedIn
           </button>
         </a>
-        <a href="https://github.com/meganveldhuis" className="gradient-box">
+        <a
+          href="https://github.com/meganveldhuis"
+          className="gradient-box gradient-box--pink"
+        >
           <button className="hero__button">
             <img src={githubLogo} />
             View my GitHub
