4c99e0b198bae01aa1b2a6c52ed47d81e787dbc4
Megan Veldhuis <megan.veldhuis@gmail.com>
2025-05-09 16:41:04 -0600

    start project tiles

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,11 +1,12 @@
+import "./ProjectSection.scss";
 import projects from "../../data/projects.json";
 import ProjectTile from "../ProjectTile/ProjectTile";
 
 function ProjectSection() {
   return (
-    <section id="projects">
-      <h2>FEATURED PROJECTS</h2>
-      <ul>
+    <section id="projects" className="projects">
+      <h2 className="projects__header">FEATURED PROJECTS</h2>
+      <ul className="projects__list">
         {projects.map((project) => (
           <ProjectTile project={project} key={project.id} />
         ))}
--- src/components/ProjectTile/ProjectTile.jsx
+++ src/components/ProjectTile/ProjectTile.jsx
@@ -1,16 +1,17 @@
 import "./ProjectTile.scss";
+import photo from "../../../public/images/reflowOven.png";
 
 function ProjectTile({ project }) {
   return (
     <li className="project" key={project.id}>
-      <img className="project__img" />
+      {/* https://www.youtube.com/watch?v=NWG1Ygt1k1k&ab_channel=WebStylePress */}
+      <img className="project__img" src={photo} alt={project.img} />
       <h3 className="project__title">{project.title}</h3>
-      <p className="project__description">{project.description}</p>
-      <a href={project.link}>{project.link}</a>
-      <p>{project.context}</p>
+      <p className="project__context">{project.context}</p>
       <p>
         {project.startDate} - {project.endDate}
       </p>
+      <p className="project__description">{project.description}</p>
       <ul className="project__techstack">
         {project.techStack.map((tech, index) => (
           <li className="project__tech-item" key={index}>
@@ -18,6 +19,9 @@ function ProjectTile({ project }) {
           </li>
         ))}
       </ul>
+      <a className="project__link" href={project.link}>
+        {project.link}
+      </a>
     </li>
   );
 }
