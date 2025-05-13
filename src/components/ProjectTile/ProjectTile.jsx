83d6bc8069efe3dfb941e16ac11a22677889cfb5
Megan Veldhuis <megan.veldhuis@gmail.com>
2025-05-12 22:53:49 -0600

    fixed project images

--- src/components/ProjectTile/ProjectTile.jsx
+++ src/components/ProjectTile/ProjectTile.jsx
@@ -1,11 +1,10 @@
 import "./ProjectTile.scss";
-import photo from "../../../public/images/reflowOven.png";
+// import photo from "../../../public/images/reflowOven.png";
 
 function ProjectTile({ project }) {
   return (
     <li className="project" key={project.id}>
-      {/* https://www.youtube.com/watch?v=NWG1Ygt1k1k&ab_channel=WebStylePress */}
-      <img className="project__img" src={photo} alt={project.img} />
+      <img className="project__img" src={project.img} alt={project.img} />
       <h3 className="project__title">{project.title}</h3>
       <p className="project__context">{project.context}</p>
       <p>
@@ -19,8 +18,11 @@ function ProjectTile({ project }) {
           </li>
         ))}
       </ul>
-      <a className="project__link" href={project.link}>
-        {project.link}
+      <a
+        href={project.link}
+        className="gradient-box gradient-box--blue project__button"
+      >
+        <button className="project__link">View Project on GitHub</button>
       </a>
     </li>
   );
